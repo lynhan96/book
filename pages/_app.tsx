8cd653850947885129171ee59dfdfab6f92d8bd1
Elvis Lee <lynhan15041997@gmail.com>
2022-06-22 20:14:39 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,5 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import '@react-page/editor/lib/index.css';
-// import plugin css. We recommend to do that
-// where you import the plugin itself,
-// but there is a problem currently preventing us from do so,
-// but that only applies to this example project
-// see https://github.com/vercel/next.js/issues/19717
 import '@react-page/plugins-background/lib/index.css';
 import '@react-page/plugins-html5-video/lib/index.css';
 import '@react-page/plugins-spacer/lib/index.css';
@@ -15,7 +9,8 @@ import '@react-page/plugins-slate/lib/index.css';
 import 'katex/dist/katex.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const PageComponent = Component as any;
+  return <PageComponent {...pageProps} />;
 }
 
 export default MyApp;
